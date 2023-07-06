098c65ffc32bf8debbe6335094697879db46b7ff
mpoots <m.poots69@gmail.com>
2023-07-06 19:52:06 +0200

    update implementation

--- src/main/java/com/example/oauth2demo/Application.java
+++ src/main/java/com/example/oauth2demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OAuth2DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(OAuth2DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/example/oauth2demo/controller/HomeController.java
+++ src/main/java/com/example/oauth2demo/controller/HomeController.java
@@ -1,2 +1,18 @@
-package com.example.oauth2demo.controller;public class HomeController {
+package com.example.oauth2demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/private")
+    public String privateArea() {
+        return "Private area";
+    }
 }
